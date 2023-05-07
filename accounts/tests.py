520901e7fa271f6e435d928178205afeb052c4bc
Aditya <adityasoniprofessional@gmail.com>
2023-05-07 18:09:36 +0530

    Wrote test cases for signup

---
+++ accounts/tests.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+# Create your tests here.
+
+class SignUpPageTest(TestCase):
+    def test_url_exist_at_correct_location(self):
+        response = self.client.get("/accounts/signup")
+        self.assertEqual(response.status_code , 200)
+    def test_signup_template(self):
+        response = self.client.get(reverse("signup"))
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response,"registration/signup.html")
+    def test_signup_form(self):
+        response = self.client.post(
+            reverse('signup'),
+            {
+                "username" : "testuser",
+                "password1" : "mr7R^Mzh&6RL&mv$",
+                "password2" : "mr7R^Mzh&6RL&mv$"
+            }
+        )
+        
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(get_user_model().objects.all().count(),1)
+        self.assertEqual(get_user_model().objects.all()[0].username,"testuser")
+    
+        
